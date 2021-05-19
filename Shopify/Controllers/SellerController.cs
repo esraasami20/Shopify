243e6b7ecc95a9661700c6ee01fce2b8011a54ea
Esraa Sami <74627798+esraasami20@users.noreply.github.com>
2021-05-19 22:58:14 +0200

    f

---
+++ Shopify/Controllers/SellerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopify.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellerController : ControllerBase
+    {
+        [HttpGet]
+        public string ggg()
+        {
+            return "gg";
+        }
+       
+    }
+}
