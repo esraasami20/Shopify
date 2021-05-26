021edef6d036d37786f8dbf83f7a4daa92505ab3
Esraa Sami <74627798+esraasami20@users.noreply.github.com>
2021-05-26 10:33:35 +0200

    updatedb

--- Shopify/Models/Governorate.cs
+++ Shopify/Models/Governorate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -21,7 +22,8 @@ namespace Shopify.Models
 
         [Required]
         public float ShippingValue { get; set; }
-
+        [DefaultValue(false)]
+        public bool Isdeleted { get; set; }
 
         public virtual List<ShippingDetail> ShippingDetails { get; set; }
 
---
+++ Shopify/Repository/StatusRepo.cs
@@ -0,0 +1,17 @@
+﻿using Shopify.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopify.Repository
+{
+    public class StatusRepo
+    {
+        ShopifyContext _db;
+        public StatusRepo(ShopifyContext db)
+        {
+            _db = db;
+        }
+    }
+}
